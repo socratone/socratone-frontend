1d368d0608141ff112c8be3ec5a63fcbf4457572
socratone <gim2origin@gmail.com>
2020-12-29 21:29:01 +0900

    logo title 추가

---
+++ src/components/nav/Item.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Item = ({ children }) => {
+  return (  
+    <li>
+      {children}
+    </li>
+  );
+}
+ 
+export default Item;
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import LogoIcon from '../icon/logoIcon';
+import Item from './Item';
+import styles from './Nav.module.scss';
 
 const Nav = () => {
   return (  
-    <nav>
-      
+    <nav className={styles.nav}>
+      <a className={styles.title}>
+        <p className={styles.titleIcon}>
+          <LogoIcon size="34" color="#f5f5f5" />
+        </p>
+        <p className={styles.titleText}>SCRTONE</p>
+      </a>
+      <Item>
+      </Item>
     </nav>
   );
 }
