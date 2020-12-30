ea65e252932d0e2b18783f38704d5c933b85b77a
socratone <gim2origin@gmail.com>
2020-12-30 13:45:37 +0900

    Nav에 반응형 적용

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import LogoIcon from '../icon/logoIcon';
 import Item from './Item';
+import MenuIcon from '../icon/MenuIcon';
 import styles from './Nav.module.scss';
 
 const Nav = () => {
   return (  
-    <nav className={styles.nav}>
-      <a className={styles.title}>
-        <p className={styles.titleIcon}>
-          <LogoIcon size="34" color="#f5f5f5" />
-        </p>
-        <p className={styles.titleText}>SCRTONE</p>
-      </a>
-      <ul className={styles.itemWrap}>
-        <Item>PORTFOLIO</Item>
-        <Item>ABOUT</Item>
-        <Item>CONTACT</Item>
-      </ul>
-    </nav>
+    <section className={styles.navWrap}>
+      <nav className={styles.nav}>
+        <a className={styles.title}>
+          <p className={styles.titleIcon}>
+            <LogoIcon size="34" color="#f5f5f5" />
+          </p>
+          <p className={styles.titleText}>SCRTONE</p>
+        </a>
+        <ul className={styles.itemWrap}>
+          <Item>PORTFOLIO</Item>
+          <Item>ABOUT</Item>
+          <Item>CONTACT</Item>
+        </ul>
+        <div className={styles.menu}>
+          <MenuIcon size="20" color="#f5f5f5" />
+        </div>
+      </nav>
+    </section>
   );
 }
  
