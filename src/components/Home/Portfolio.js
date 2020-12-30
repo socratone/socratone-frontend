23a2afb0bf898da216e3719b15d95292182f770b
socratone <gim2origin@gmail.com>
2020-12-30 16:28:26 +0900

    Portfolio Item 추가

--- src/components/Home/Portfolio.js
+++ src/components/Home/Portfolio.js
@@ -19,6 +19,19 @@ const Portfolio = () => {
         description="도자기로 성물을 만드는 김지영 작가님의 홈페이지입니다."
         image="https://picsum.photos/640/480" 
       />
+      <PortfolioItem 
+        subTitle="Electron App"
+        title="Youtube Music Player"
+        description="유튜브 음악 영상을 재생리스트에 넣고 음악을 재생하는 플레이어 앱입니다."
+        image="https://picsum.photos/560/420" 
+      />
+      <PortfolioItem 
+        isEven
+        subTitle="Electron App"
+        title="손가락 오르가니스트"
+        description="리듬에 맞춰 손가락을 터치해 가톨릭 성가 반주를 해주는 앱입니다."
+        image="https://picsum.photos/480/360" 
+      />
     </main>
   );
 }
